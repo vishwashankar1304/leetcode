8c6d95e480fc0f43c0604f5cdf808e7c756c1621
Vishwa S <vishwashankar1304@gmail.com>
2025-09-05 19:52:00 +0530

    Time: 3 ms (19.18%), Space: 55.5 MB (61.98%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,35 @@
 class Solution {
-    public int[] productExceptSelf(int[] arr) {
-        int n=arr.length;
-        if(n==2)return new int[] {arr[1],arr[0]};
-         int[] pre = new int[n];
-        int[] pos = new int[n];
-         Arrays.fill(pre,1);
-        Arrays.fill(pos,1);
-        pre[0]=1;
-         for(int i=1;i<n;i++){
-            pre[i]=pre[i-1]*arr[i-1];
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+    	// 	for (int i = 0; i < nums.length; i++) {
+		//     int prefix = 1;
+		//     int suffix = 1;
+            
+		//     for (int j = 0; j < i; j++) {
+		//         prefix *= nums[j];
+		//     }
+
+		//     for (int j = i + 1; j < nums.length; j++) {
+		//         suffix *= nums[j];
+		//     }
+		    
+		//     output[i] = prefix * suffix;
+		  
+		// }
+
+output[0]=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            output[i]=output[i-1]*nums[i-1];
         }
-        pos[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            pos[i]=pos[i+1]*arr[i+1];
+
+int s=1;
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            output[i]=output[i]*s;
+            s*=nums[i];
         }
-        int[] res=new int[n];
-        for(int i=0;i<n;i++){
-            res[i]=pre[i]*pos[i];
-        }
-        for(int i=0;i<n;i++)System.out.print(res[i]+" ");
-        return res;
+        return output;
 
     }
 }
